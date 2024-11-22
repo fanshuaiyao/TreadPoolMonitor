0af4bbed6c02b6a02913de0b27477709c5c2e9d7
fanshuaiyao <1345822123@qq.com>
2024-11-22 14:11:03 +0800

    初始化项目，一个简单的springBoot项目，加入了web依赖和lombok依赖

---
+++ src/main/java/com/fan/threadpoolmonitor/ThreadPoolMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.fan.threadpoolmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadPoolMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadPoolMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fan/threadpoolmonitor/ThreadPoolMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fan.threadpoolmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadPoolMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
